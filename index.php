f28e21067b1c74d275b9d5d505c74c95e82e0f62
Nikita <nik.korkuts@mail.ru>
2023-03-04 17:17:07 +0300

    delete composer

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>ToDo List</title>
 	<link rel="stylesheet" href="assets/css/style.css">
-	<link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <body>
 	<form action="handler.php" method="post">
